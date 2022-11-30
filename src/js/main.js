9aa31572c165bea299756ad7aa341da2661d171c
PerpetuumG <perpetuumg@gmail.com>
2022-11-30 10:06:57 +0200

    add server json

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -12,6 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
   sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
   forms();
   mask('[name = "phone"]');
-  checkTextInputs('[name = "name"]')
-  checkTextInputs('[name = "message"]')
+  checkTextInputs('[name = "name"]');
+  checkTextInputs('[name = "message"]');
+  showMoreStyles('.button-styles', '#styles .row');
 });
